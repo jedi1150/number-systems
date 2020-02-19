9de68051410300bcee93497d8a63263ad56b755a
jedi1150 <6olegarh6@gmail.com>
2020-02-19 13:47:27 +0300

    fixes

--- app/src/main/java/ru/sandello/binaryconverter/TypeMethod.kt
+++ app/src/main/java/ru/sandello/binaryconverter/TypeMethod.kt
@@ -2,13 +2,11 @@ package ru.sandello.binaryconverter
 
 import android.text.InputType
 import android.text.method.DigitsKeyListener
-import android.util.Log
 import android.widget.EditText
 
 class TypeMethod {
     fun type(editText: EditText, allowVal: String?) {
-        Log.d("length", allowVal!!.length.toString())
-        if (allowVal.length <= 10) {
+        if (allowVal!!.length <= 10) {
             editText.inputType = InputType.TYPE_CLASS_NUMBER
             editText.keyListener = DigitsKeyListener.getInstance("1234567890,.-")
         }
