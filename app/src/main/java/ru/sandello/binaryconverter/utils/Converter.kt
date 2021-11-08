dca7f961ba6526c467e4a1debfe8e98bb599f228
Oleg Yakovlev <yaoleg96@gmail.com>
2021-11-08 19:54:55 +0300

    Converter logic improved

--- app/src/main/java/ru/sandello/binaryconverter/utils/Converter.kt
+++ app/src/main/java/ru/sandello/binaryconverter/utils/Converter.kt
@@ -1,28 +1,18 @@
 package ru.sandello.binaryconverter.utils
 
 import android.util.Log
+import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.flow.flowOn
 import ru.sandello.binaryconverter.model.ConvertedData
 import java.util.*
 import kotlin.math.pow
 
 class Converter {
 
-    private var string = ""
-    private var leftLength = 0 //Длина целой части числа
-    private var result = 0.toBigDecimal() //Переменная суммы
-    private var returnString = "" //Возвращаемая строка
-    private var fromParse = 0
-    private var toParse = 0
-
-    private var decArray = charArrayOf()
-    private var leftDecArray = charArrayOf()
-    private var rightDecArray = charArrayOf()
-    private var minusBool = false
-
     operator fun invoke(value: String, fromRadix: Int, toRadix: Int): Flow<ConvertedData> = flow {
-        Log.d(APP_TAG, "Converter:: invoke")
+        Log.d(APP_TAG, "Converter::invoke")
 
         assert(fromRadix > 2 || fromRadix < 36 || toRadix > 2 || fromRadix < 36) {
             "Radix must be greater than 2 and smaller than 36"
@@ -34,114 +24,71 @@ class Converter {
         }
 
         emit(ConvertedData(result = convert(value, fromRadix, toRadix), fromRadix = fromRadix, toRadix = toRadix))
-    }
+    }.flowOn(Dispatchers.Default)
 
     private fun convert(value: String, fromRadix: Int, toRadix: Int): String {
-        Log.d(APP_TAG, "Converter:: convert: fromRadix: $fromRadix, toRadix: $toRadix")
+        Log.d(APP_TAG, "Converter::convert: fromRadix: $fromRadix, toRadix: $toRadix")
 
+        var minusBool = false
         if (value.contains("-")) {
-            string = value.replace("-", "")
+            value.replace("-", "")
             minusBool = true
-        } else string = value
-        fromParse = fromRadix
-        toParse = toRadix
-
-        leftDecArray = string.split("[,.]".toRegex())[0].toCharArray()
-        try {
-            rightDecArray = string.split("[,.]".toRegex())[1].toCharArray()
-        } catch (e: Exception) {
         }
-        decArray = string.replace("[,.]".toRegex(), "").toCharArray() //Строка без разделителя
 
-        if (fromParse != 10 && toParse == 10) returnString = toDec()
-        if (fromParse == 10 && toParse != 10) returnString = fromDec()
-        if (fromParse == 10 && toParse == 10) returnString = fromDec()
-        if (fromParse != 10 && toParse != 10) returnString = notDec()
+        var result: String
+
+        if (fromRadix != 10) {
+            result = toDec(value, fromRadix)
+            if (toRadix != 10) {
+                result = fromDec(result, toRadix)
+            }
+        } else {
+            result = fromDec(value, toRadix)
+        }
+
         if (minusBool) {
-            returnString = returnString.replaceRange(0, 0, "-")
-            minusBool = false
+            result = result.replaceRange(0, 0, "-")
         }
-        return returnString
+        return result
     }
 
-    private fun toDec(): String {
-        Log.d(APP_TAG, "Converter:: toDec")
+    private fun toDec(value: String, fromRadix: Int): String {
+        Log.d(APP_TAG, "Converter::toDec")
 
-        leftLength = leftDecArray.size
-        for (i in decArray) {
-            leftLength--
-            result += i.toString().toInt(fromParse).toString(10).toBigDecimal() *
-                    fromParse.toDouble().pow(leftLength.toDouble()).toBigDecimal()
+        val valueWithoutComma = value.replace("[,.]".toRegex(), "")
+        val integerPart = value.split("[,.]".toRegex())[0]
+
+        val dec = valueWithoutComma.toCharArray().mapIndexed { index, char ->
+            char.toString().toInt(fromRadix).toString(10).toBigDecimal() *
+                    fromRadix.toDouble().pow(integerPart.toCharArray().size - (index + 1)).toBigDecimal()
         }
-        while ((result.toString().length > 1 && result.toString()
-                .contains("[,.]".toRegex()) && result.toString().endsWith("0")) || result.toString()
-                .endsWith(".") || result.toString().endsWith(",")
-        ) {
-            result = result.toString().substring(0, result.toString().length - 1).toBigDecimal()
+        var result = dec.reduce { acc, decimal -> acc + decimal }.toString()    // Summing all chars
+
+        // Pretty formatting
+        while (result.length > 1 && result.contains("[,.]".toRegex()) && result.endsWith("0")) {
+            result = result.substring(0, result.length - 1)
         }
-        return result.toString()
+
+        return result
     }
 
-    private fun fromDec(): String {
-        Log.d(APP_TAG, "Converter:: fromDec")
+    private fun fromDec(value: String, toRadix: Int): String {
+        Log.d(APP_TAG, "Converter::fromDec")
 
-        var res = ""
-        leftDecArray = string.split("[,.]".toRegex())[0].toCharArray()
-        try {
-            rightDecArray = string.split("[,.]".toRegex())[1].toCharArray()
-        } catch (e: Exception) {
-        }
-
-        var left = ""
-        for (i in leftDecArray) {
-            left += i.toString()
-        }
-        try {
-            res = left.toBigInteger(10).toString(toParse)
-                .uppercase(Locale.getDefault()) //Перевод основной части числа в 10
-        } catch (e: Exception) {
-        }
-        if (rightDecArray.isNotEmpty()) {
-            var right = ""
-            for (i in rightDecArray) {
-                right += i.toString()
+        var result: String = value.split("[,.]".toRegex())[0].toBigInteger(10).toString(toRadix).uppercase(Locale.getDefault())
+        if ((value.contains(",") || value.contains(".")) && value.split("[,.]".toRegex())[1].isNotEmpty()) {
+            var fractionalPart = value
+            var i = 0
+            var formattedFraction = ""
+            while (i < Shared.FRACTION_COUNT) {
+                if (fractionalPart.split("[,.]".toRegex())[1].toBigDecimal() == "0".toBigDecimal()) break
+                fractionalPart = (".${fractionalPart.split("[,.]".toRegex())[1]}".toBigDecimal() * toRadix.toBigDecimal()).toString() //Умножаем дробную часть на степень
+                i++
+                formattedFraction += fractionalPart.split("[,.]".toRegex())[0].toBigInteger(10).toString(toRadix).uppercase(Locale.getDefault())
             }
-            var z = 0
-            var n1 = "0.$right"
-            right = ""
-            while (z < Shared.FRACTION_COUNT) {
-
-                if (n1.contains("[,.]".toRegex())) {
-                    n1 =
-                        ("0.${n1.split("[,.]".toRegex())[1]}".toBigDecimal() * toParse.toBigDecimal()).toString() //Умножаем дробную часть на степень
-                    if (!n1.contains("[,.]".toRegex())) break
-                } else
-                    break
-                right += if (toParse < 11) {
-                    n1.split("[,.]".toRegex())[0]  //Целую часть от каждой итерации приписываем к строке
-                } else {
-                    n1.split("[,.]".toRegex())[0].toBigInteger(10).toString(toParse)
-                        .uppercase(Locale.getDefault()) //Целую часть от каждой итерации приписываем к строке
-                }
-                z++
-            }
-            res += ".$right"
-
-            while ((res.length > 1 && res.contains("[,.]".toRegex()) && res.endsWith("0")) || res.endsWith(
-                    "."
-                ) || res.endsWith(",")
-            ) {
-                res = res.substring(0, res.length - 1)
-            }
+            result += ".$formattedFraction"
         }
-        return res
+        return result
     }
 
-    private fun notDec(): String {
-        Log.d(APP_TAG, "Converter:: notDec")
-
-        string = toDec()
-        fromParse = 10
-        return fromDec()
-    }
 }
