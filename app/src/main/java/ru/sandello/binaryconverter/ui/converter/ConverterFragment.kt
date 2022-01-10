bbd23a1b1bbd1c6932ee8841564152f4794c321f
Oleg Yakovlev <yaoleg96@gmail.com>
2022-01-11 01:56:39 +0300

    removed unused LOC

--- app/src/main/java/ru/sandello/binaryconverter/ui/converter/ConverterFragment.kt
+++ app/src/main/java/ru/sandello/binaryconverter/ui/converter/ConverterFragment.kt
@@ -1,27 +1,18 @@
 package ru.sandello.binaryconverter.ui.converter
 
-import android.content.Context
 import android.os.Bundle
-import android.text.Editable
-import android.text.TextWatcher
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.navigation.navGraphViewModels
 import com.google.android.material.bottomsheet.BottomSheetBehavior
-import com.google.android.material.bottomsheet.BottomSheetBehavior.*
+import com.google.android.material.bottomsheet.BottomSheetBehavior.STATE_HIDDEN
 import com.google.android.material.bottomsheet.BottomSheetDialog
 import ru.sandello.binaryconverter.R
-import ru.sandello.binaryconverter.databinding.ActivityMainBinding
 import ru.sandello.binaryconverter.databinding.FragmentConverterBinding
-import ru.sandello.binaryconverter.utils.TypeMethod
-
-var cleared = false
 
 class ConverterFragment : Fragment() {
-    private lateinit var activityBinding: ActivityMainBinding
     private lateinit var binding: FragmentConverterBinding
     private val model: ConverterViewModel by navGraphViewModels(R.id.nav_graph)
 
@@ -32,7 +23,6 @@ class ConverterFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?,
     ): View {
-//        activityBinding = (requireActivity() as MainActivity).binding
         binding = FragmentConverterBinding.inflate(layoutInflater)
         binding.model = model
         binding.lifecycleOwner = viewLifecycleOwner
@@ -41,7 +31,6 @@ class ConverterFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-//        model.load()
         bottomSheetDialog = BottomSheetDialog(view.rootView.context)
         bottomSheetDialog!!.setContentView(R.layout.fragment_explanation)
         bottomSheetInternal = bottomSheetDialog!!.findViewById(R.id.bottomSheetMain)
@@ -56,185 +45,6 @@ class ConverterFragment : Fragment() {
             }
 
             override fun onSlide(bottomSheet: View, slideOffset: Float) {}
-        })
-
-        activityBinding.clearFab.setOnClickListener {
-            binding.editText10.setText("")
-            binding.editText2.setText("")
-            binding.editText8.setText("")
-            binding.editText16.setText("")
-            binding.editTextCustom.setText("")
-
-            activityBinding.clearFab.hide()
-            activityBinding.explanationFab.hide()
-        }
-
-        model.operandBase.observe(viewLifecycleOwner, {
-//            Log.d("test228 operandCustom observe", it.toString())
-        })
-
-//        TypeMethod().type(
-//            binding.editTextCustom,
-//            model.allow(binding.fractionCustom.text.toString().toInt())
-//        )
-
-        binding.fractionCustom.setOnItemClickListener { _, _, position, _ ->
-            try {
-                TypeMethod().type(
-                    binding.editTextCustom,
-                    model.allow(binding.fractionCustom.text.toString().toInt())
-                )
-                model.updateCustomRadix(position)
-//                binding.editTextCustom.setText(
-//                    ConvertTo().main(
-//                        binding.editText10.text.toString(),
-//                        10,
-//                        listCustomBin[position]
-//                    )
-//                )
-                binding.editTextCustom.setSelection(binding.editTextCustom.text!!.length)
-                errorNull()
-            } catch (e: Exception) {
-                if (binding.editTextCustom.text.toString() != "")
-                    binding.textInputLayoutCustom.error = getString(R.string.invalid_value)
-            }
-        }
-
-        val spinnerSharedPref =
-            requireContext().getSharedPreferences("spinner", Context.MODE_PRIVATE)
-        val spinnerEditor = spinnerSharedPref.edit()
-
-//        var adapter = ArrayAdapter(
-//            requireContext(),
-//            R.layout.dropdown_menu_popup_item,
-//            listAllBin
-//        )
-
-        //10
-/*        binding.editText10.addTextChangedListener(object : TextWatcher {
-            override fun afterTextChanged(s: Editable?) {}
-            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
-            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
-                Log.d("test228 onTextChanged", s.toString())
-                model.updateOperand(fraction = 10, value = s.toString())
-//                if (binding.editText10.hasFocus()) {
-//                    if (s.toString() != "" && !s!!.endsWith("."))
-//                        parse(
-//                            s.toString(),
-//                            binding.editText10,
-//                            binding.textInputLayout10,
-//                            10,
-//                            binding.fractionCustom.text.toString().toInt(),
-//                            model.allow(10)
-//                        )
-//                    if (s.toString() == "" && !cleared)
-//                        clearConverter(binding.editText10)
-//                }
-//                binding.editText10.removeTextChangedListener(this)
-//                Format().format(binding.editText10)
-//                binding.editText10.addTextChangedListener(this)
-            }
-        })*/
-        //2
-/*        binding.editText2.addTextChangedListener(object : TextWatcher {
-            override fun afterTextChanged(s: Editable?) {}
-            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
-            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
-                model.updateOperand(fraction = 2, value = s.toString())
-//                if (binding.editText2.hasFocus())
-//                    if (s.toString() != "" && !s!!.endsWith("."))
-//                        parse(
-//                            s.toString(),
-//                            binding.editText2,
-//                            binding.textInputLayout2,
-//                            2,
-//                            binding.fractionCustom.text.toString().toInt(),
-//                            model.allow(2)
-//                        )
-//                if (s.toString() == "" && !cleared)
-//                    clearConverter(binding.editText2)
-//                binding.editText2.removeTextChangedListener(this)
-//                Format().format(binding.editText2)
-//                binding.editText2.addTextChangedListener(this)
-            }
-        })*/
-        //8
- /*       binding.editText8.addTextChangedListener(object : TextWatcher {
-            override fun afterTextChanged(s: Editable?) {}
-            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
-            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
-                model.updateOperand(fraction = 8, value = s.toString())
-
-//                if (binding.editText8.hasFocus())
-//                    if (s.toString() != "" && !s!!.endsWith("."))
-//                        parse(
-//                            s.toString(),
-//                            binding.editText8,
-//                            binding.textInputLayout8,
-//                            8,
-//                            binding.fractionCustom.text.toString().toInt(),
-//                            model.allow(8)
-//                        )
-//                if (s.toString() == "" && !cleared)
-//                    clearConverter(binding.editText8)
-//                binding.editText8.removeTextChangedListener(this)
-//                Format().format(binding.editText8)
-//                binding.editText8.addTextChangedListener(this)
-            }
-        })*/
-        //16
-        binding.editText16.addTextChangedListener(object : TextWatcher {
-            override fun afterTextChanged(s: Editable?) {}
-            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
-            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
-//                model.updateOperand(fraction = 16, value = s.toString())
-//                if (binding.editText16.hasFocus())
-//                    if (s.toString() != "" && !s!!.endsWith(".") && binding.editText16.hasFocus())
-//                        parse(
-//                            s.toString(),
-//                            binding.editText16,
-//                            binding.textInputLayout16,
-//                            16,
-//                            binding.fractionCustom.text.toString().toInt(),
-//                            model.allow(16)
-//                        )
-//                if (s.toString() == "" && !cleared)
-//                    clearConverter(binding.editText16)
-//                binding.editText16.removeTextChangedListener(this)
-//                Format().format(binding.editText16)
-//                binding.editText16.addTextChangedListener(this)
-            }
-        })
-        //custom
-        binding.editTextCustom.addTextChangedListener(object : TextWatcher {
-            override fun afterTextChanged(s: Editable?) {}
-            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
-            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
-                Log.d("test228 custom", s.toString())
-                if (s != "" && s != null) {
-                    model.fractionCustom.value?.let {
-//                        model.updateOperand(
-//                            fraction = it,
-//                            value = s.toString()
-//                        )
-                    }
-                }
-//                binding.editTextCustom.hasFocus()
-//                if (s.toString() != "" && !s!!.endsWith(".") && binding.editTextCustom.hasFocus())
-//                    parse(
-//                        s.toString(),
-//                        binding.editTextCustom,
-//                        binding.textInputLayoutCustom,
-//                        binding.fractionCustom.text.toString().toInt(),
-//                        null,
-//                        model.allow(binding.fractionCustom.text.toString().toInt())
-//                    )
-//                if (s.toString() == "")
-//                    clearConverter(binding.editTextCustom)
-//                binding.editTextCustom.removeTextChangedListener(this)
-//                Format().format(binding.editTextCustom)
-//                binding.editTextCustom.addTextChangedListener(this)
-            }
         })
 
         /*activityBinding.explanationFab.setOnClickListener {
@@ -285,66 +95,4 @@ class ConverterFragment : Fragment() {
         }*/
     }
 
-
-    fun clearConverter(editField: View?) {
-        if (!cleared) {
-            cleared = true
-            if (editField?.resources?.getResourceEntryName(editField.id) != "editText10") binding.editText10.setText(
-                ""
-            )
-            if (editField?.resources?.getResourceEntryName(editField.id) != "editText2") binding.editText2.setText(
-                ""
-            )
-            if (editField?.resources?.getResourceEntryName(editField.id) != "editText8") binding.editText8.setText(
-                ""
-            )
-            if (editField?.resources?.getResourceEntryName(editField.id) != "editText16") binding.editText16.setText(
-                ""
-            )
-            if (editField?.resources?.getResourceEntryName(editField.id) != "editTextCustom") binding.editTextCustom.setText(
-                ""
-            )
-        }
-        errorNull()
-        activityBinding.clearFab.hide()
-        activityBinding.explanationFab.hide()
-    }
-
-    private fun errorNull() {
-        binding.textInputLayout10.error = null
-        binding.textInputLayout2.error = null
-        binding.textInputLayout8.error = null
-        binding.textInputLayout16.error = null
-        binding.textInputLayoutCustom.error = null
-    }
-
-    private fun checkClear() {
-        if (binding.editText10.text.toString() != "" || binding.editText2.text.toString() != "" || binding.editText8.text.toString() != "" || binding.editText16.text.toString() != "" || binding.editTextCustom.text.toString() != "") {
-            activityBinding.clearFab.show()
-            if (binding.editText10.text.toString() != "1") {
-                activityBinding.explanationFab.show()
-            } else {
-                activityBinding.explanationFab.show()
-            }
-        } else {
-            activityBinding.clearFab.hide()
-            activityBinding.explanationFab.hide()
-            binding.editText10.setText("")
-            binding.editText2.setText("")
-            binding.editText8.setText("")
-            binding.editText16.setText("")
-            binding.editTextCustom.setText("")
-        }
-    }
-
-
-    override fun onStart() {
-        super.onStart()
-//        model.load()
-    }
-
-    override fun onPause() {
-        super.onPause()
-//        model.save()
-    }
 }
--- app/src/main/java/ru/sandello/binaryconverter/ui/converter/ConverterViewModel.kt
+++ app/src/main/java/ru/sandello/binaryconverter/ui/converter/ConverterViewModel.kt
@@ -4,14 +4,14 @@ import android.util.Log
 import androidx.compose.runtime.State
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.ui.text.input.TextFieldValue
-import androidx.lifecycle.*
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import kotlinx.coroutines.FlowPreview
 import kotlinx.coroutines.flow.*
 import kotlinx.coroutines.launch
 import ru.sandello.binaryconverter.utils.APP_TAG
 import ru.sandello.binaryconverter.utils.CharRegex
 import ru.sandello.binaryconverter.utils.Shared.converter
-import java.math.BigDecimal
 
 class ConverterViewModel : ViewModel() {
 
@@ -36,65 +36,6 @@ class ConverterViewModel : ViewModel() {
 
     private val radixes = IntArray(36) { it + 1 }
     val customRadixes = radixes.toMutableList().filter { !listOf(1, 2, 8, 10, 16).contains(it) }
-
-    private val _operandBase = MediatorLiveData<BigDecimal>()
-    val operandBase: LiveData<BigDecimal> = _operandBase
-
-    val operandTest = MutableLiveData<String>()
-    private val _operand10 = MutableLiveData(0.toBigDecimal())
-    val operand10: LiveData<BigDecimal> = _operand10
-    private val _operand2 = MutableLiveData(0.toBigDecimal())
-    val operand2: LiveData<BigDecimal> = _operand2
-    private val _operand8 = MutableLiveData(0.toBigDecimal())
-    val operand8: LiveData<BigDecimal> = _operand8
-    private val _operand16 = MutableLiveData(0.toBigDecimal())
-    val operand16: LiveData<BigDecimal> = _operand16
-    val _operandCustom = MutableLiveData(0.toBigDecimal())
-    val operandCustom: LiveData<BigDecimal> = _operandCustom
-    private val _fractionCustom = MutableLiveData(10)
-    val fractionCustom: LiveData<Int> = _fractionCustom
-
-    val showInvalidInputError = MutableLiveData<Pair<Int, String>>()
-    val stringToast = MutableLiveData<String>()
-
-    private val sym = arrayOf(
-        0,
-        1,
-        2,
-        3,
-        4,
-        5,
-        6,
-        7,
-        8,
-        9,
-        "A",
-        "B",
-        "C",
-        "D",
-        "E",
-        "F",
-        "G",
-        "H",
-        "I",
-        "J",
-        "K",
-        "L",
-        "M",
-        "N",
-        "O",
-        "P",
-        "Q",
-        "R",
-        "S",
-        "T",
-        "U",
-        "V",
-        "W",
-        "X",
-        "Y",
-        "Z",
-    )
 
     @OptIn(FlowPreview::class)
     fun convert(textFieldValue: TextFieldValue, fromRadix: Int) {
@@ -160,57 +101,6 @@ class ConverterViewModel : ViewModel() {
         _customRadix.value = newRadix
     }
 
-    /*private fun parse() {
-        if (_operandBase.value != null) {
-            GlobalScope.launch(Dispatchers.Main) {
-                withContext(Dispatchers.Main) {
-                    viewModelScope.launch {
-                        val resultTest =
-                            withContext(Dispatchers.Default) {
-                                ConvertTo().main(
-                                    _operandBase.value.toString(),
-                                    10,
-                                    2
-                                )
-                            }
-                        Log.d("test228 1", resultTest)
-                        Log.d("test228 2", operandTest.value.toString())
-                        if (resultTest != operandTest.value.toString()
-                        ) {
-                            operandTest.postValue(resultTest)
-                        }
-                    }
-                }
-            }
-//            _operand10.postValue(
-//                ConvertTo().main(_operandBase.value.toString(), 10, 10).toBigDecimal()
-//            )
-            _operand2.postValue(
-                ConvertTo().main(operandBase.value.toString(), 10, 2).toBigDecimal()
-            )
-            _operand8.postValue(
-                ConvertTo().main(operandBase.value.toString(), 10, 8).toBigDecimal()
-            )
-//            _operand16.postValue(
-//                ConvertTo().main(_operandBase.value.toString(), 10, 16).toBigDecimal()
-//            )
-            _operandCustom.postValue(
-                ConvertTo().main(
-                    _operand10.value.toString(),
-                    10,
-                    fractionCustom.value
-                ).toBigDecimal()
-            )
-        }
-    }*/
-
-    fun allow(to: Int): String {
-        var allowVal = ""
-        for (i in 0 until to)
-            allowVal += sym[i]
-        return allowVal
-    }
-
     fun clear() {
         _operand10new.value = TextFieldValue()
         _operand2new.value = TextFieldValue()
@@ -219,45 +109,4 @@ class ConverterViewModel : ViewModel() {
         _operandCustomNew.value = TextFieldValue()
     }
 
-/*    fun load() {
-        Log.d(
-            "test228",
-            (Shared.preferencesHelper.getStringFromPrefs(SHARED_CONV_OPERAND)?.toBigDecimal()
-                ?: 0.toBigDecimal().toString()).toString()
-        )
-        _operandBase.postValue(
-            Shared.preferencesHelper.getStringFromPrefs(SHARED_CONV_OPERAND)?.toBigDecimal()
-                ?: 0.toBigDecimal()
-        )
-        Log.d("test228", _operand10.value.toString())
-
-        _fractionCustom.postValue(
-            Shared.preferencesHelper.getIntFromPrefs(
-                SHARED_CONV_FRACTION_CUSTOM
-            )
-        )
-
-//        filled_exposed_dropdown.setText(converterSave.getInt("converterSpinner", 3).toString())
-//        filled_exposed_dropdown.setAdapter(adapter)
-//        if (editText10.text.toString() != "")
-//            parse(
-//                converterSave.getString("converter10", "")!!,
-//                requireView().rootView.editText10,
-//                requireView().rootView.textInputLayout10,
-//                10,
-//                filled_exposed_dropdown.text.toString().toInt(),
-//                "0123456789"
-//            )
-//        checkClear()
-    }*/
-
-    /*fun save() {
-        Shared.preferencesHelper.stringToPrefs(SHARED_CONV_OPERAND, operand10.value.toString())
-        fractionCustom.value?.let {
-            Shared.preferencesHelper.intToPrefs(
-                SHARED_CONV_FRACTION_CUSTOM,
-                it
-            )
-        }
-    }*/
 }
