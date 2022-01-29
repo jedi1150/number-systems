db19a70e93cf8304cdd3b975eb95a6a4b049c159
Oleg Yakovlev <yaoleg96@gmail.com>
2022-01-29 17:25:04 +0300

    Dependencies updated

--- app/src/main/java/ru/sandello/binaryconverter/ui/theme/Theme.kt
+++ app/src/main/java/ru/sandello/binaryconverter/ui/theme/Theme.kt
@@ -28,8 +28,15 @@ private val LightColorPalette = lightColors(
 )
 
 @Composable
-fun NumberSystemsTheme(darkTheme: Boolean = isSystemInDarkTheme(), content: @Composable() () -> Unit) {
-    val colors = if (darkTheme) { DarkColorPalette } else { LightColorPalette }
+fun NumberSystemsTheme(
+    darkTheme: Boolean = isSystemInDarkTheme(),
+    content: @Composable () -> Unit
+) {
+    val colors = if (darkTheme) {
+        DarkColorPalette
+    } else {
+        LightColorPalette
+    }
 
     MaterialTheme(
         colors = colors,
