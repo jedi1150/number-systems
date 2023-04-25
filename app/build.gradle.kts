a8762cce59855207415dfbe60af0081419461c15
Oleg Yakovlev <yaoleg96@gmail.com>
2023-04-25 23:39:55 +0300

    versionCode updated

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,7 +14,7 @@ android {
         applicationId = "ru.sandello.binaryconverter"
         minSdk = 21
         targetSdk = 33
-        versionCode = 114
+        versionCode = 115
         versionName = "2.0.0"
         vectorDrawables {
             useSupportLibrary = true
