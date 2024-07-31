14405032c4651561ce3fb0361106628eaff6c40c
Oleg Yakovlev <yaoleg96@gmail.com>
2024-07-31 12:20:40 +0300

    App version increased

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -16,7 +16,7 @@ android {
         minSdk = 21
         targetSdk = 34
         versionCode = 1
-        versionName = "2.2.1"
+        versionName = "2.2.2"
     }
     buildTypes {
         named("release") {
