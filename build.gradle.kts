52887cfdf4e190d069aa36cacaa1eda47d5d57ad
Oleg Yakovlev <yaoleg96@gmail.com>
2026-09-07 22:36:12 +0300

    remove kotlin-android plugin

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,7 +7,6 @@ plugins {
     alias(libs.plugins.hilt) apply false
     alias(libs.plugins.ksp) apply false
     alias(libs.plugins.kotlin.jvm) apply false
-    alias(libs.plugins.kotlin.android) apply false
     alias(libs.plugins.kotlin.serialization) apply false
     alias(libs.plugins.baselineprofile) apply false
     alias(libs.plugins.androidTest) apply false
